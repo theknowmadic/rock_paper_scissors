7fe59f978f287223e397d0270bdfa9aaa9691611
theknowmad <127548786+theknowmadic@users.noreply.github.com>
2023-03-25 17:15:35 -0400

    Completed program. Refactoring

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-let playerSore = 0
+let playerScore = 0
 let compScore = 0
 
 // Function makes the computer generate either rock, paper, or scissors
-function getComputerChoice() {
+const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random()*3)
     switch (randomNumber) {
         case 0:
@@ -15,39 +15,54 @@ function getComputerChoice() {
 }
 
 
-    // Function that plays a single round of the game against the computer 
-    function playRound(playerSelection, computerSelection) {
+// Function that plays a single round of the game against the computer 
+    const playRound = (playerSelection, computerSelection) => {
 
         if (playerSelection === "rock" && computerSelection === "rock") {
             return "It is a tie! Rock against rock"
         } else if (playerSelection === "rock" && computerSelection === "paper") {
+            compScore++
             return "You loose! Paper beats rock"
         } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore++
             return "You win! Rock beats scissors"
         } else if (playerSelection === "paper" && computerSelection === "paper") {
             return "It is a tie! Paper against paper"
         } else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore++
             return "You win! Paper beats rock"
         } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            compScore++
             return "You loose! Scissors beats paper"
         } else if (playerSelection === "scissors" && computerSelection === "scissors") {
             return "It is a tie! Scissors against scissors"
         } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            compScore++
             return "You loose! Rock beats scissors"
         } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore++
             return "You win! Scissors beat paper"
         } 
 
     } 
  
 
-const playerSelection = prompt("Please enter:").toLowerCase();
-
+// Function with a for-loop running the game 5 times
 const game = () => {
     for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Please Choose: ", "Rock, Paper, or Scissors").toLowerCase()
         const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection)
+        console.log(playRound(playerSelection, computerSelection))
+    }
+
+// Conditional to determine who won
+if (playerScore > compScore) {
+    return "You beat the computer!"
+} else if (playerScore < compScore) {
+    return "You got beat!"
+} else {
+    return "You got tied!"
     }
 }
 
-game()
+console.log(game())
